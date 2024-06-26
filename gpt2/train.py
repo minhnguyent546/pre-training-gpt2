fix: scale gradient accum step as we do not use `DistributedSampler`

gpt2/train.py
@@ -168,7 +168,7 @@ def train_model(config: dict):
 
     train_iter = tqdm(
         range(initial_step, train_steps),
-        desc=f'Training model on gpu {config["local_rank"]}',
+        desc=f'Training model on gpu{config.get(" local_rank", "")}',
         disable=config['local_rank'] != 0,
     )
     torch.cuda.empty_cache()
@@ -271,9 +271,8 @@ def main():
         init_process_group(backend='nccl')  # nccl, gloo, etc
 
         # scale down the gradient accumulation step
-        # do we need this?
-        # assert config['gradient_accum_step'] % config['world_size'] == 0
-        # config['gradient_accum_step'] //= config['world_size']
+        assert config['gradient_accum_step'] % config['world_size'] == 0
+        config['gradient_accum_step'] //= config['world_size']
 
         # add offset for seed
         config['seed'] += config['rank']