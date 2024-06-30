161a9b3bd633a14c352dcd37db43e558634c8e1f
minhnguyent546 <minhnguyent546@gmail.com>
2024-06-30 18:23:57 +0700

    feat: add tone normalization in `gpt2/utils.py`

--- gpt2/utils.py
+++ gpt2/utils.py
@@ -3,6 +3,7 @@ import random
 import regex
 import unicodedata
 import yaml
+from typing import Any
 
 import numpy as np
 
@@ -22,7 +23,7 @@ def load_yaml_config(config_path: str):
         config = yaml.safe_load(config_file)
     return config
 
-def chunks(data: list | str, chunk_size: int = 1_000):
+def chunks(data: list[Any] | str, chunk_size: int = 1_000):
     for i in range(0, len(data), chunk_size):
         yield data[i:i+chunk_size]
 
@@ -90,6 +91,65 @@ def clean_text(text: str, *, strip: bool = True, keep_punct: bool = True) -> str
         text = regex.sub(r'([^\p{Latin}\p{Punctuation}0-9\s]+)', r'', text)
     else:
         text = regex.sub(r'([^\p{Latin}0-9\s]+)', r'', text)
+    # normalize tone
+    text = normalize_tone(text)
     if strip:
         text = text.strip()
+    return text
+
+
+tone_normalization_map = {
+    "òa": "oà",
+    "Òa": "Oà",
+    "ÒA": "OÀ",
+    "óa": "oá",
+    "Óa": "Oá",
+    "ÓA": "OÁ",
+    "ỏa": "oả",
+    "Ỏa": "Oả",
+    "ỎA": "OẢ",
+    "õa": "oã",
+    "Õa": "Oã",
+    "ÕA": "OÃ",
+    "ọa": "oạ",
+    "Ọa": "Oạ",
+    "ỌA": "OẠ",
+    "òe": "oè",
+    "Òe": "Oè",
+    "ÒE": "OÈ",
+    "óe": "oé",
+    "Óe": "Oé",
+    "ÓE": "OÉ",
+    "ỏe": "oẻ",
+    "Ỏe": "Oẻ",
+    "ỎE": "OẺ",
+    "õe": "oẽ",
+    "Õe": "Oẽ",
+    "ÕE": "OẼ",
+    "ọe": "oẹ",
+    "Ọe": "Oẹ",
+    "ỌE": "OẸ",
+    "ùy": "uỳ",
+    "Ùy": "Uỳ",
+    "ÙY": "UỲ",
+    "úy": "uý",
+    "Úy": "Uý",
+    "ÚY": "UÝ",
+    "ủy": "uỷ",
+    "Ủy": "Uỷ",
+    "ỦY": "UỶ",
+    "ũy": "uỹ",
+    "Ũy": "Uỹ",
+    "ŨY": "UỸ",
+    "ụy": "uỵ",
+    "Ụy": "Uỵ",
+    "ỤY": "UỴ",
+}
+
+def normalize_tone(text: str) -> str:
+    """
+    Tone normalization for Vietnamese (source: https://github.com/VinAIResearch/BARTpho/blob/main/VietnameseToneNormalization.md)
+    """
+    for orig, repl in tone_normalization_map.items():
+        text = text.replace(orig, repl)
     return text
