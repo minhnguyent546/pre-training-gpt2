2f456da5411acc12890475dcfc57855dd8186dbc
minhnguyent546 <minhnguyent546@gmail.com>
2024-07-12 17:02:35 +0700

    feat: tokenize tokens with pool.imap instead of pre-preprocess to save space

--- scripts/prepare_fineweb_edu.py
+++ scripts/prepare_fineweb_edu.py
@@ -2,12 +2,17 @@
 
 """
 Download and tokenize the fineweb-edu dataset (10BT subset) (https://huggingface.co/datasets/HuggingFaceFW/fineweb-edu)
-for pre-training GPT2.
+for pre-training GPT2. Adapted from https://github.com/karpathy/build-nanogpt.
+
+This script will need approximately 80GiB space on disk to download and process
+the dataset, plus 20GiB for tokens files.
 """
 
 import argparse
+import multiprocessing as mp
 import os
 import random
+from functools import partial
 from typing import Any
 
 import datasets
@@ -17,14 +22,16 @@ from tqdm.autonotebook import tqdm
 
 
 def prepare_fineweb_edu(args: argparse.Namespace) -> None:
+    UINT16_LIMIT = 2 ** 16 - 1
+
     # set seed
     random.seed(args.seed)
     np.random.seed(args.seed)
 
     # tokenizer
     tokenizer = tiktoken.encoding_for_model(args.tokenizer)
-    eot_id = tokenizer.eot_token
-    dump_id = -1
+    dump_id = tokenizer.n_vocab
+    assert dump_id <= UINT16_LIMIT
 
     output_dir = args.output_dir
     os.makedirs(output_dir, exist_ok=True)
@@ -39,59 +46,56 @@ def prepare_fineweb_edu(args: argparse.Namespace) -> None:
         cache_dir=args.cache_dir,
     )
 
-    def tokenize_examples(examples: dict[str, list[Any]]) -> dict[str, list[Any]]:
-        tokens_list = tokenizer.encode_ordinary_batch(examples['text'], num_threads=num_workers)
-        tokens_list = [tokens + [eot_id] for tokens in tokens_list]
-        # lengths = [len(tokens) for tokens in tokens_list]
-        return {
-            'tokens': tokens_list,
-            # 'length': lengths,
-        }
-
     ds = ds.shuffle(seed=args.seed)
-    ds = ds.map(tokenize_examples, batched=True, num_proc=num_workers, remove_columns=['text'])
     train_ds = ds['train']
-    # total_tokens = sum(train_ds['length'])
-    # print('Total tokens:', total_tokens)
 
     shard_size = args.shard_size
     shard_idx = 0
     token_count = 0
     arr = np.full((shard_size,), dump_id, dtype=np.uint16)
+    num_approx_shards = 10 * 10**9 // shard_size
+    print(f'Approximately {num_approx_shards} shards will be created with size {shard_size} tokens each')
     progress_bar = tqdm(desc=f'Processing shard {shard_idx}', total=shard_size, unit=' tokens')
-    for i in range(len(train_ds)):
-        tokens = train_ds[i]['tokens']
-        assert all(token < 2**16 for token in tokens)
-        cur_num_tokens = len(tokens)
-        if cur_num_tokens + token_count > shard_size:
-            arr[token_count:] = tokens[:shard_size - token_count]
-            progress_bar.update(shard_size - token_count)
-            num_remain_tokens = cur_num_tokens - (shard_size - token_count)
+    with mp.Pool(num_workers) as pool:
+        for item in pool.imap(partial(tokenize_example, tokenizer=tokenizer), train_ds, chunksize=32):
+            tokens = item['tokens']
+            cur_num_tokens = len(tokens)
+            if cur_num_tokens + token_count > shard_size:
+                arr[token_count:] = tokens[:shard_size - token_count]
+                progress_bar.update(shard_size - token_count)
+                num_remain_tokens = cur_num_tokens - (shard_size - token_count)
 
-            # save to .npy file
+                # save to .npy file
+                file_path = os.path.join(output_dir, f'fineweb_edu_{shard_idx:04d}.npy')
+                np.save(file_path, arr)
+                progress_bar.write(f'Saved shard {shard_idx} to {file_path}')
+                progress_bar = None
+
+                # add remain tokens to arr for the next shard
+                shard_idx += 1
+                progress_bar = tqdm(desc=f'Processing shard {shard_idx}', total=shard_size, unit=' tokens')
+                arr.fill(dump_id)
+                arr[:num_remain_tokens] = tokens[shard_size - token_count:]
+                token_count = num_remain_tokens
+                progress_bar.update(num_remain_tokens)
+
+            else:
+                arr[token_count:token_count+cur_num_tokens] = tokens
+                token_count += cur_num_tokens
+                progress_bar.update(cur_num_tokens)
+
+        if token_count > 1:
             file_path = os.path.join(output_dir, f'fineweb_edu_{shard_idx:04d}.npy')
-            assert all(0 <= token < 2**16 for token in arr)  # ensure that we don't miss any position in the array
-            np.save(file_path, arr)
+            np.save(file_path, arr[:token_count])
             progress_bar.write(f'Saved shard {shard_idx} to {file_path}')
-            progress_bar = None
 
-            # add remain tokens to arr for the next shard
-            shard_idx += 1
-            progress_bar = tqdm(desc=f'Processing shard {shard_idx}', total=shard_size, unit=' tokens')
-            arr.fill(dump_id)
-            arr[:num_remain_tokens] = tokens[shard_size - token_count:]
-            token_count = num_remain_tokens
-            progress_bar.update(num_remain_tokens)
-
-        else:
-            arr[token_count:token_count+cur_num_tokens] = tokens
-            token_count += cur_num_tokens
-            progress_bar.update(cur_num_tokens)
-
-    if token_count > 1:
-        file_path = os.path.join(output_dir, f'fineweb_edu_{shard_idx:04d}.npy')
-        np.save(file_path, arr[:token_count])
-        progress_bar.write(f'Saved shard {shard_idx} to {file_path}')
+def tokenize_example(example: dict[str, Any], tokenizer: tiktoken.Encoding) -> dict[str, Any]:
+    tokens = tokenizer.encode_ordinary(example['text'])
+    tokens.append(tokenizer.eot_token)
+    return {
+        'tokens': tokens,
+        'length': len(tokens),
+    }
 
 def main():
     parser = argparse.ArgumentParser(
