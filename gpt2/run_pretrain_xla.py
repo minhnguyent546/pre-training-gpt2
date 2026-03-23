f7fd213d3d5cce5b25f88c27b2fde2f14d3af182
minhnguyent546 <minhnguyent546@gmail.com>
2026-03-24 02:05:13 +0700

    fix: correct logging grad norm value

--- gpt2/run_pretrain_xla.py
+++ gpt2/run_pretrain_xla.py
@@ -356,6 +356,7 @@ def train_model(args: argparse.Namespace):
         })
         wandb_accum_logs[-1].update({
             "loss/batch_loss": batch_loss,
+            "grad_norm": grad_norm_value,
             "step": global_step,
         })
 
@@ -377,7 +378,6 @@ def train_model(args: argparse.Namespace):
             wandb_accum_logs[-1].update({
                 "loss/train": running_loss.average,
                 "loss/valid": valid_results["loss"],
-                "grad_norm": grad_norm_value,
             })
             running_loss.reset()
 
